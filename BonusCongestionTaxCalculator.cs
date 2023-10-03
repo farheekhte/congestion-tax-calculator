cc1615e747d70460d88eab1d71142c0ed91e97eb
Ali Sheikh Nezami | علی شیخ نظامی <farheekhte@gmail.com>
2023-10-03 20:44:16 +0330

    # Congestion Tax Calculator
    # Congestion Tax Calculator
    
    This is a C# application for calculating congestion tax fees for vehicles within the Gothenburg area. The project aims to determine the total congestion tax for vehicles based on the dates and times they pass through specific toll stations.
    
    ## The Scenario
    
    Your junior colleague started working on this application, but unfortunately, they have gone on parental leave, leaving the project incomplete. This repository contains the initial codebase with some bugs in the calculation and architecture. Your task is to refactor and complete the code to make it correct and maintainable.
    
    ## Assignment
    
    Your mission is to refactor and improve the existing code to meet the following requirements:
    
    - Correctly calculate the total congestion tax for vehicles based on the provided rules.
    - Ensure the code is clean, well-organized, and easy to maintain.
    - Implement the functionality to handle multiple cities with different congestion tax rules.
    - Utilize external data stores for storing and retrieving congestion tax parameters to allow for easy updates by content editors.
    
    ## Usage
    
    To use this application, follow these steps:
    
    1. Clone this repository to your local machine.
    
    2. Open the solution in your preferred C# IDE (e.g., Visual Studio).
    
    3. Build the solution to ensure all dependencies are resolved.
    
    4. You can create instances of different types of vehicles (e.g., Car, Motorbike) that implement the `IVehicle` interface and pass them along with a list of DateTime objects representing the dates and times the vehicle passed through toll stations.
    
    5. Call the `CalculateTotalTax` method on an instance of `CongestionTaxCalculator`, passing the vehicle and date/time information as well as the city for which you want to calculate the congestion tax.
    
    6. The method will return the total congestion tax amount for the specified vehicle and city.
    
    7. Ensure that you have the correct parameters for each city in the `_cityTollFees` dictionary or use an external data store as mentioned in the bonus scenario.
    
    ## Congestion Tax Rules
    
    - The maximum daily congestion tax per vehicle is 60 SEK.
    - Congestion tax is not charged on weekends (Saturdays and Sundays), public holidays, days before a public holiday, and during the month of July.
    - Different time intervals have different tax amounts, as specified in the code.
    
    ## Bonus Scenario
    
    The codebase is designed to be extensible for handling multiple cities with different congestion tax rules. To achieve this:
    
    1. Move the parameters used by the application to an external data store or configuration file.
    
    2. Implement a mechanism to read these parameters during runtime, allowing different content editors to update the tax rules for various cities.
    
    3. Ensure that the application can handle congestion tax calculations for multiple cities seamlessly.
    
    Feel free to contribute to the project by improving the code, adding features, or providing suggestions for enhancements.
    
    ## License
    
    This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.

---
+++ BonusCongestionTaxCalculator.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using congestion.calculator;
+
+public class BonusCongestionTaxCalculator
+{
+    /**
+         * Calculate the total toll fee for one day
+         *
+         * @param vehicle - the vehicle
+         * @param dates   - date and time of all passes on one day
+         * @return - the total congestion tax for that day
+         */
+
+    // List of dates that are toll-free (e.g., public holidays, weekends)
+    private readonly List<DateTime> _tollFreeDates = new List<DateTime>
+    {
+        // Add dates exempt from congestion tax
+        new DateTime(2013, 1, 1),
+        new DateTime(2013, 3, 28),
+        new DateTime(2013, 3, 29),
+        // ... Add more dates here ...
+    };
+
+    // Default toll fees based on time intervals
+    private readonly Dictionary<TimeSpan, int> _defaultTollFees = new Dictionary<TimeSpan, int>
+    {
+        // Define toll fees based on time intervals
+        { new TimeSpan(6, 0, 0), 8 },
+        { new TimeSpan(6, 30, 0), 13 },
+        { new TimeSpan(7, 0, 0), 18 },
+        { new TimeSpan(8, 0, 0), 13 },
+        { new TimeSpan(15, 0, 0), 13 },
+        { new TimeSpan(15, 30, 0), 18 },
+        { new TimeSpan(17, 0, 0), 13 },
+        { new TimeSpan(18, 0, 0), 8 },
+        { TimeSpan.FromDays(1), 0 } // For unspecified times
+    };
+
+    // Toll fees for different cities
+    private readonly Dictionary<string, Dictionary<TimeSpan, int>> _cityTollFees = new Dictionary<string, Dictionary<TimeSpan, int>>
+    {
+        {
+            "Gothenburg", new Dictionary<TimeSpan, int>
+            {
+                // Define toll fees for Gothenburg based on time intervals
+                { new TimeSpan(6, 0, 0), 8 },
+                { new TimeSpan(6, 30, 0), 13 },
+                { new TimeSpan(7, 0, 0), 18 },
+                { new TimeSpan(8, 0, 0), 13 },
+                { new TimeSpan(15, 0, 0), 13 },
+                { new TimeSpan(15, 30, 0), 18 },
+                { new TimeSpan(17, 0, 0), 13 },
+                { new TimeSpan(18, 0, 0), 8 },
+                { TimeSpan.FromDays(1), 0 } // For unspecified times
+            }
+        },
+        {
+            "Stockholm", new Dictionary<TimeSpan, int>
+            {
+                // Define toll fees for Stockholm based on time intervals
+                { new TimeSpan(6, 0, 0), 10 },
+                { new TimeSpan(6, 30, 0), 15 },
+                { new TimeSpan(7, 0, 0), 20 },
+                { new TimeSpan(8, 0, 0), 15 },
+                { new TimeSpan(15, 0, 0), 15 },
+                { new TimeSpan(15, 30, 0), 20 },
+                { new TimeSpan(17, 0, 0), 15 },
+                { new TimeSpan(18, 0, 0), 10 },
+                { TimeSpan.FromDays(1), 0 } // For unspecified times
+            }
+        },
+        // Add more cities and their toll fees here...
+    };
+
+    // Method to calculate the total congestion tax for a vehicle for a list of dates in a specific city
+    public int CalculateTotalTax(Vehicle vehicle, List<DateTime> dates, string city)
+    {
+        // Check if the vehicle is toll-free or if the city is invalid
+        if (IsTollFreeVehicle(vehicle) || !IsValidCity(city))
+            return 0;
+
+        int totalTax = 0;
+        DateTime intervalStart = dates[0];
+        var tollFees = GetTollFeesForCity(city);
+
+        foreach (DateTime date in dates)
+        {
+            int nextTax = GetTollFee(date.TimeOfDay, tollFees);
+            int tempTax = GetTollFee(intervalStart.TimeOfDay, tollFees);
+
+            if (IsWithinSameHour(date, intervalStart))
+                totalTax = CalculateHourlyTax(totalTax, tempTax, nextTax);
+            else
+                totalTax += nextTax;
+
+            intervalStart = date;
+        }
+
+        // Ensure the total tax does not exceed the maximum of 60 SEK
+        return Math.Min(totalTax, 60);
+    }
+
+    // Check if two DateTime instances are within the same hour
+    private bool IsWithinSameHour(DateTime date1, DateTime date2)
+    {
+        TimeSpan timeDifference = date1 - date2;
+        return timeDifference.TotalMinutes <= 60;
+    }
+
+    // Calculate the total tax when within the same hour
+    private int CalculateHourlyTax(int totalTax, int tempTax, int nextTax)
+    {
+        if (totalTax > 0) totalTax -= tempTax;
+        if (nextTax >= tempTax) tempTax = nextTax;
+        return totalTax + tempTax;
+    }
+
+    // Check if the vehicle is toll-free
+    private bool IsTollFreeVehicle(Vehicle vehicle)
+    {
+        if (vehicle == null) return false;
+        string vehicleType = vehicle.GetVehicleType();
+        return IsTollFreeVehicleType(vehicleType);
+    }
+
+    // Check if a vehicle type string is toll-free
+    private bool IsTollFreeVehicleType(string vehicleType)
+    {
+        return Enum.GetNames(typeof(TollFreeVehicles)).Contains(vehicleType);
+    }
+
+    // Check if the city is valid
+    private bool IsValidCity(string city)
+    {
+        return _cityTollFees.ContainsKey(city);
+    }
+
+    // Get toll fees for a specific city
+    private Dictionary<TimeSpan, int> GetTollFeesForCity(string city)
+    {
+        return _cityTollFees[city];
+    }
+
+    // Get the toll fee based on the time of day and toll fees dictionary
+    private int GetTollFee(TimeSpan timeOfDay, Dictionary<TimeSpan, int> tollFees)
+    {
+        if (IsTollFreeTime(timeOfDay)) return 0;
+
+        var fee = tollFees.FirstOrDefault(f => timeOfDay <= f.Key);
+        return fee.Value;
+    }
+
+    // Check if the time of day is toll-free
+    private bool IsTollFreeTime(TimeSpan timeOfDay)
+    {
+        return timeOfDay >= new TimeSpan(18, 30, 0) || timeOfDay < new TimeSpan(6, 0, 0);
+    }
+
+    // Enum for toll-free vehicle types
+    private enum TollFreeVehicles
+    {
+        Motorcycle,
+        Tractor,
+        Emergency,
+        Diplomat,
+        Foreign,
+        Military
+    }
+}
---
+++ CongestionTaxCalculator.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace congestion.calculator
+{
+    public class CongestionTaxCalculator
+    {
+        private readonly List<DateTime> _tollFreeDates = new List<DateTime>
+        {
+            // تاریخ‌های معاف از مالیات ترافیکی را اضافه کنید
+            new DateTime(2013, 1, 1),
+            new DateTime(2013, 3, 28),
+            new DateTime(2013, 3, 29),
+            // ... Add more dates here ...
+        };
+
+        private readonly Dictionary<TimeSpan, int> _tollFees = new Dictionary<TimeSpan, int>
+        {
+            // محاسبه قیمت‌های مالیات ترافیکی بر اساس بازه زمانی
+            { new TimeSpan(6, 0, 0), 8 },
+            { new TimeSpan(6, 30, 0), 13 },
+            { new TimeSpan(7, 0, 0), 18 },
+            { new TimeSpan(8, 0, 0), 13 },
+            { new TimeSpan(15, 0, 0), 13 },
+            { new TimeSpan(15, 30, 0), 18 },
+            { new TimeSpan(17, 0, 0), 13 },
+            { new TimeSpan(18, 0, 0), 8 },
+            { TimeSpan.FromDays(1), 0 } // برای اوقاتی که تعریف نشده‌اند
+        };
+
+        private readonly Dictionary<string, Dictionary<TimeSpan, int>> _cityTollFees = new Dictionary<string, Dictionary<TimeSpan, int>>
+        {
+            {
+                "Gothenburg", new Dictionary<TimeSpan, int>
+                {
+                    // محاسبه قیمت‌های مالیات ترافیکی بر اساس بازه زمانی برای Gothenburg
+                    { new TimeSpan(6, 0, 0), 8 },
+                    { new TimeSpan(6, 30, 0), 13 },
+                    { new TimeSpan(7, 0, 0), 18 },
+                    { new TimeSpan(8, 0, 0), 13 },
+                    { new TimeSpan(15, 0, 0), 13 },
+                    { new TimeSpan(15, 30, 0), 18 },
+                    { new TimeSpan(17, 0, 0), 13 },
+                    { new TimeSpan(18, 0, 0), 8 },
+                    { TimeSpan.FromDays(1), 0 } // برای اوقاتی که تعریف نشده‌اند
+                }
+            },
+            {
+                "Stockholm", new Dictionary<TimeSpan, int>
+                {
+                    // محاسبه قیمت‌های مالیات ترافیکی بر اساس بازه زمانی برای Stockholm
+                    { new TimeSpan(6, 0, 0), 10 },
+                    { new TimeSpan(6, 30, 0), 15 },
+                    { new TimeSpan(7, 0, 0), 20 },
+                    { new TimeSpan(8, 0, 0), 15 },
+                    { new TimeSpan(15, 0, 0), 15 },
+                    { new TimeSpan(15, 30, 0), 20 },
+                    { new TimeSpan(17, 0, 0), 15 },
+                    { new TimeSpan(18, 0, 0), 10 },
+                    { TimeSpan.FromDays(1), 0 } // برای اوقاتی که تعریف نشده‌اند
+                }
+            },
+            // اضافه کردن شهرهای دیگر و قوانین مالیات ترافیکی آن‌ها اینجا...
+        };
+
+        public int CalculateTotalTax(Vehicle vehicle, List<DateTime> dates, string city)
+        {
+            // بررسی اگر خودرو معاف از مالیات ترافیکی باشد یا شهر نامعتبر باشد
+            if (IsTollFreeVehicle(vehicle) || !IsValidCity(city))
+                return 0;
+
+            int totalTax = 0;
+            DateTime intervalStart = dates[0];
+            var tollFees = GetTollFeesForCity(city);
+
+            foreach (DateTime date in dates)
+            {
+                int nextTax = GetTollFee(date.TimeOfDay, tollFees);
+                int tempTax = GetTollFee(intervalStart.TimeOfDay, tollFees);
+
+                if (IsWithinSameHour(date, intervalStart))
+                    totalTax = CalculateHourlyTax(totalTax, tempTax, nextTax);
+                else
+                    totalTax += nextTax;
+
+                intervalStart = date;
+            }
+
+            // اطمینان حاصل شود که مجموع مالیات به حداکثر 60 SEK نرسد
+            return Math.Min(totalTax, 60);
+        }
+
+        private bool IsWithinSameHour(DateTime date1, DateTime date2)
+        {
+            TimeSpan timeDifference = date1 - date2;
+            return timeDifference.TotalMinutes <= 60;
+        }
+
+        private int CalculateHourlyTax(int totalTax, int tempTax, int nextTax)
+        {
+            if (totalTax > 0) totalTax -= tempTax;
+            if (nextTax >= tempTax) tempTax = nextTax;
+            return totalTax + tempTax;
+        }
+
+        private bool IsTollFreeVehicle(Vehicle vehicle)
+        {
+            if (vehicle == null) return false;
+            string vehicleType = vehicle.GetVehicleType();
+            return IsTollFreeVehicleType(vehicleType);
+        }
+
+        private bool IsTollFreeVehicleType(string vehicleType)
+        {
+            return Enum.GetNames(typeof(TollFreeVehicles)).Contains(vehicleType);
+        }
+
+        private bool IsValidCity(string city)
+        {
+            return _cityTollFees.ContainsKey(city);
+        }
+
+        private Dictionary<TimeSpan, int> GetTollFeesForCity(string city)
+        {
+            return _cityTollFees[city];
+        }
+
+        private int GetTollFee(TimeSpan timeOfDay, Dictionary<TimeSpan, int> tollFees)
+        {
+            if (IsTollFreeTime(timeOfDay)) return 0;
+
+            var fee = tollFees.FirstOrDefault(f => timeOfDay <= f.Key);
+            return fee.Value;
+        }
+
+        private bool IsTollFreeTime(TimeSpan timeOfDay)
+        {
+            return timeOfDay >= new TimeSpan(18, 30, 0) || timeOfDay < new TimeSpan(6, 0, 0);
+        }
+
+        private enum TollFreeVehicles
+        {
+            Motorcycle,
+            Tractor,
+            Emergency,
+            Diplomat,
+            Foreign,
+            Military
+        }
+    }
+}
